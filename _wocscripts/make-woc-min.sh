d58cc81096670da08dfce2fc822c56f391e312f7
tarh <tarh.x2@gmail.com>
2014-07-20 18:17:06 +0100

    Add the polyfill for ES6 promises to woc.min.js

--- _wocscripts/make-woc-min.sh
+++ _wocscripts/make-woc-min.sh
@@ -2,5 +2,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
-uglifyjs woc.w/comptree.js woc.w/contexts.js woc.w/loader.js woc.w/loader-w.js woc.w/main.js  woc.w/serv-ajax.js  woc.w/serv-log.js  woc.w/serv-router.js  woc.w/utils.js -o woc.min.js
+uglifyjs woc.w/comptree.js woc.w/contexts.js woc.w/loader.js woc.w/loader-w.js woc.w/main.js  woc.w/serv-ajax.js  woc.w/serv-log.js  woc.w/serv-router.js  woc.w/utils.js woc.w/lib/promise-1.0.0.min.js -o woc.min.js
 
