8cd2a0daf8102d48e2e0fcbfb010d3a186f6f80a
paleo <contact@paleo.casa>
2017-10-16 19:56:35 +0100

    Clean unused code

--- bkb-core/src/main.ts
+++ bkb-core/src/main.ts
@@ -1,14 +1,1 @@
-export {
-  ComponentEvent,
-  Transmitter,
-  ParentFilter,
-  FindChildOptions,
-  EmitterOptions,
-  Dash,
-  PublicDash,
-  LogItem,
-  Log,
-  ApplicationDash
-} from "./interfaces"
-
 export { createApplication, asApplication } from "./Application"
