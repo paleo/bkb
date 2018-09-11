5f1470c9602a957a8dc4170ea5fc8928cb92c343
paleo <contact@paleo.casa>
2018-09-11 18:34:20 +0100

    Dash create

--- src/exported-definitions.d.ts
+++ src/exported-definitions.d.ts
@@ -176,12 +176,12 @@ export interface Dash<A = any> extends PublicDash<A> {
   /**
    * Create a child component by instantiating the `Class` without options.
    */
-  create<C, D extends Dash = Dash<A>>(Class: { new (dash: D): C }): C
+  create<C = any, D extends Dash = Dash<A>>(Class: { new (dash: D): C }): C
 
   /**
    * Create a child component by instantiating the `Class`. The options will be passed to the constructor at second constructor parameter.
    */
-  create<C, OPT, D extends Dash = Dash<A>>(Class: { new (dash: D, options: OPT): C }, options: OPT): C
+  create<C = any, OPT = any, D extends Dash = Dash<A>>(Class: { new (dash: D, options: OPT): C }, options: OPT): C
 
   /**
    * Make the `obj` a child component.
