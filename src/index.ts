13367dbe60d9aabe3b27c14fb43465752e1a063a
paleo <contact@paleo.casa>
2018-09-27 09:40:03 +0100

    'createApplication' can be called without options

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { AppBkb } from "./AppBkb"
 import { AppDash } from "./exported-definitions"
 
-export function createApplication<A, O>(Class: { new(dash: AppDash<A>, options: O): A }, options: O): A {
+export function createApplication<A, O>(Class: { new (dash: AppDash<A>): A }): A
+export function createApplication<A, O>(Class: { new (dash: AppDash<A>, options: O): A }, options: O): A
+export function createApplication(Class: { new(dash: AppDash, options?) }, options?) {
   let bkb = new AppBkb(Class, false, options)
   return bkb.root.getInstance() as any
 }
